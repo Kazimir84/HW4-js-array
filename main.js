4f2a2d3e9b3bb2466ccea039ef6a4b47a11dd0eb
Kazimir84 <kav2007@ukr.net>
2021-07-04 20:54:15 +0300

    Add to first(4) task first function is arrow function

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
 console.log('------------- # 4')
-function ask(question, yes, no) {
+// function ask(question, yes, no) {
+//     if (confirm(question)) yes()
+//     else no();
+//   };
+
+let ask = (question, yes, no) => {
     if (confirm(question)) yes()
     else no();
   };
